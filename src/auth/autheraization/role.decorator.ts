64fe6d849705516ce21bda9217d6747edd571f90
Youssef <yyygfhc@gmail.com>
2025-09-14 22:11:36 +0300

    Remove old backend files from root

--- src/auth/autheraization/role.decorator.ts
+++
@@ -1,6 +0,0 @@
-
-import { SetMetadata } from '@nestjs/common';
-import { Role, RoleWithAdmin } from 'src/DTOs/DTOs';
-
-export const ROLES_KEY = 'roles';
-export const Roles = (...roles: RoleWithAdmin[]) => SetMetadata(ROLES_KEY, roles);
